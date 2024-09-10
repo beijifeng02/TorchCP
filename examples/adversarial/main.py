b2da39f1ba5b12a9cb1c872efba0e0738ea4a93b
33798 <3379854453@qq.com>
2024-09-10 10:15:21 +0800

    msg

--- examples/adversarial/main.py
+++ examples/adversarial/main.py
@@ -32,10 +32,10 @@ rows = x_test.size()[2]
 cols = x_test.size()[3]
 channels = x_test.size()[1]
 
-x_test_adv = Smooth_Adv_ImageNet(model, x_test, y_test, indices, cfg["n_smooth"]["value"], sigma_smooth,
-            N_steps=cfg["N_steps"]["value"], max_norm=cfg["epsilon"]["value"], device=device, GPU_CAPACITY=GPU_CAPACITY)
-x_test_adv_base = Smooth_Adv_ImageNet(model, x_test, y_test, indices, 1, sigma_smooth,
-            N_steps=cfg["N_steps"]["value"], max_norm=cfg["epsilon"]["value"], device=device, GPU_CAPACITY=GPU_CAPACITY)
+x_test_adv = Smooth_Adv_ImageNet(model, testloader, indices, cfg["n_smooth"]["value"], sigma_smooth,
+                    cfg["N_steps"]["value"], cfg["epsilon"]["value"], device, GPU_CAPACITY=GPU_CAPACITY)
+x_test_adv_base = Smooth_Adv_ImageNet(model, testloader, indices, 1, sigma_smooth,
+                    cfg["N_steps"]["value"], cfg["epsilon"]["value"], device, GPU_CAPACITY=GPU_CAPACITY)
 os.makedirs(cfg["directory"]["value"])
 with open(cfg["directory"]["value"] + "/data.pickle", 'wb') as f:
     pickle.dump([x_test_adv, x_test_adv_base], f)
